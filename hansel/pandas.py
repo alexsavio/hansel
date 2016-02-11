8d08f2dbe4e8e1543a79993674d8f8214f9cad45
Alexandre Manhaes Savio <alexsavio@gmail.com>
2016-02-11 18:00:14 +0100

    add pandas helper functions

---
+++ hansel/pandas.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Utilities to fill crumbs with data from pandas DataFrames.
+#TODO: add tests
+"""
+from hansel.utils import _get_matching_items
+
+
+def _pandas_rename_cols(df, col_map):
+    """ Return a copy of `df` with the columns renamed as in `col_map`.
+    Parameters
+    ----------
+    df: pandas.DataFrame
+
+    col_map: dict[str] -> str
+        This is a "DataFrame column name" to "crumb argument name" relation
+        dictionary.
+        Example: {'Subject ID': 'subject_id'}
+
+    Returns
+    -------
+    renamed: pandas.DataFrame
+    """
+    renamed = df.copy()
+    renamed.columns = [col_map.get(col_name, col_name) for col_name in df.columns]
+    return renamed
+
+
+def values_map_from_df(df, crumb_arg_names, arg_names=None):
+    """ Return a values_map from data in `df` and
+    the matching column and arguments names from `df`, `crumb_arg_names`
+    and `arg_names`.
+    Parameters
+    ----------
+    df: pandas.DataFrame
+
+    crumb: hansel.Crumb
+
+    arg_names: sequence of str
+        A list of the crumb arguments and DataFrame columns to extract
+        the info to fill the crumbs.
+        Both must match, or use _pandas_rename_cols to rename the columns.
+        If None, will look for all the arguments that match in both
+        `df` and `arg_names`.
+        Example: ['subject_id']
+
+
+    Returns
+    -------
+    values_map: list of sequences of 2-tuple
+    """
+    crumb_names  = _get_matching_items(df.columns,
+                                       crumb_arg_names,
+                                       arg_names)
+
+    # get the columns of df that have been matched
+    return (list(rec.items()) for rec in df[crumb_names].to_dict(orient='records'))
+
+
+def pandas_fill_crumbs(df, crumb, names_map=None):
+    """ Create a generator of crumbs filled with the `df` column names and `crumb`
+    arguments that match or the ones indicated in `names_map`.
+    Parameters
+    ----------
+    df: pandas.DataFrame
+
+    crumb: hansel.Crumb
+
+    names_map: sequence of sequences of 2-tuple or dict[str] -> str
+        This is a "DataFrame column name" to "crumb argument name" relation
+        dictionary.
+        Example: {'Subject ID': 'subject_id'}
+
+    Returns
+    -------
+    crumbs: generator of crumbs
+        Crumbs filled with the data in `df`.
+    """
+    nmap = names_map
+    if not isinstance(nmap, dict):
+        nmap = dict(nmap)
+
+    return (df
+              .pipe(_pandas_rename_cols, dict(nmap))
+              .pipe(values_map_from_df, list(crumb.all_args()),
+                    arg_names=list(nmap.values()))
+            )
